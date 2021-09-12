a61caebdd5bba7c38083e229c5313c4bcc95903d
ZYROUGE <zyrouge@hotmail.com>
2021-09-12 14:59:25 +0530

    chore: update twist_moe source

--- extensions/anime/twist_moe.ts
+++ extensions/anime/twist_moe.ts
@@ -3,5 +3,5 @@ import { ExtensionConfig } from "../../lib/model";
 export const config: ExtensionConfig = {
     name: "Twist.moe",
     author: "zyrouge",
-    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/aaaef8c669c8992c18e557a42e4be7b31c53c455/extensions/anime/twist_moe.htt",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/aaaef8c669c8992c18e557a42e4be7b31c53c455/extensions/anime/twist_moe.ht",
 };
