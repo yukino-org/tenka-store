c0a59291ef0be05b6f50aa4562a011bb46ca064d
ZYROUGE <zyrouge@hotmail.com>
2021-10-13 22:02:15 +0530

    refactor: regex include decimals in title

--- extensions/manga/readm_org.ts
+++ extensions/manga/readm_org.ts
@@ -3,7 +3,7 @@ import { ExtensionConfig } from "../../lib/model";
 export const config: ExtensionConfig = {
     name: "ReadM.org",
     author: "zyrouge",
-    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/af00e2b34930ece0a1fc099d1a67f7a0e4be874e/extensions/manga/readm_org/readm_org.ht",
-    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/af00e2b34930ece0a1fc099d1a67f7a0e4be874e/extensions/manga/readm_org/readm_org.png",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/40a13db9c518025a8e0bc6be918026de7611b40c/extensions/manga/readm_org/readm_org.ht",
+    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/40a13db9c518025a8e0bc6be918026de7611b40c/extensions/manga/readm_org/readm_org.png",
     nsfw: false,
 };
