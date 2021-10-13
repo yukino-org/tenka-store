c8bf2d05fae00c3805e5b9deaf056fa9c95c8597
ZYROUGE <zyrouge@hotmail.com>
2021-10-13 21:15:08 +0530

    refactor: fix regex

--- extensions/manga/fanfox_net.ts
+++ extensions/manga/fanfox_net.ts
@@ -3,7 +3,7 @@ import { ExtensionConfig } from "../../lib/model";
 export const config: ExtensionConfig = {
     name: "FanFox.net",
     author: "zyrouge",
-    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/86dd1516d57d1cf3b6b7697eecb239c06ec86c95/extensions/manga/fanfox_net/fanfox_net.ht",
-    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/86dd1516d57d1cf3b6b7697eecb239c06ec86c95/extensions/manga/fanfox_net/fanfox_net.png",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/40a13db9c518025a8e0bc6be918026de7611b40c/extensions/manga/fanfox_net/fanfox_net.ht",
+    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/40a13db9c518025a8e0bc6be918026de7611b40c/extensions/manga/fanfox_net/fanfox_net.png",
     nsfw: false,
 };
