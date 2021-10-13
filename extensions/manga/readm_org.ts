19faf2ca5ae696db45943c294de540866c79795d
ZYROUGE <zyrouge@hotmail.com>
2021-10-13 18:32:18 +0530

    feat: add readm.org

---
+++ extensions/manga/readm_org.ts
@@ -0,0 +1,9 @@
+import { ExtensionConfig } from "../../lib/model";
+
+export const config: ExtensionConfig = {
+    name: "ReadM.org",
+    author: "zyrouge",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/1cb05997f6c1d10fc520d84b3a64c2bfb96ae3bb/extensions/manga/readm_org/readm_org.ht",
+    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/1cb05997f6c1d10fc520d84b3a64c2bfb96ae3bb/extensions/manga/readm_org/readm_org.png",
+    nsfw: false,
+};
