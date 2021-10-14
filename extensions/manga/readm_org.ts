77bdd2cb4b90719a9535a19c38c31554877cb95e
ZYROUGE <zyrouge@hotmail.com>
2021-10-14 18:26:59 +0530

    refactor: fix readm.org regex parsing

--- extensions/manga/readm_org.ts
+++ extensions/manga/readm_org.ts
@@ -3,7 +3,7 @@ import { ExtensionConfig } from "../../lib/model";
 export const config: ExtensionConfig = {
     name: "ReadM.org",
     author: "zyrouge",
-    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/40a13db9c518025a8e0bc6be918026de7611b40c/extensions/manga/readm_org/readm_org.ht",
-    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/40a13db9c518025a8e0bc6be918026de7611b40c/extensions/manga/readm_org/readm_org.png",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/7a0aa287a4e13d2243f8472af73b9a61aca428b5/extensions/manga/readm_org/readm_org.ht",
+    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/7a0aa287a4e13d2243f8472af73b9a61aca428b5/extensions/manga/readm_org/readm_org.png",
     nsfw: false,
 };
