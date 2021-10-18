f935b69549a47d3f087b9e0441daac28e45e2131
ZYROUGE <zyrouge@hotmail.com>
2021-10-18 17:47:24 +0530

    refactor(readm_org): fix thumbnail

--- extensions/manga/readm_org.ts
+++ extensions/manga/readm_org.ts
@@ -3,7 +3,7 @@ import { ExtensionConfig } from "../../lib/model";
 export const config: ExtensionConfig = {
     name: "ReadM.org",
     author: "zyrouge",
-    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/7a0aa287a4e13d2243f8472af73b9a61aca428b5/extensions/manga/readm_org/readm_org.ht",
-    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/7a0aa287a4e13d2243f8472af73b9a61aca428b5/extensions/manga/readm_org/readm_org.png",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/9f7f5e0094e555827817136d3a5706338e26f781/extensions/manga/readm_org/readm_org.ht",
+    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/9f7f5e0094e555827817136d3a5706338e26f781/extensions/manga/readm_org/readm_org.png",
     nsfw: false,
 };
