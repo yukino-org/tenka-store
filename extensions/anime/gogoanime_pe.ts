7ff5b0dcd809956a3443e94ca713755152d26705
ZYROUGE <zyrouge@hotmail.com>
2021-10-18 17:46:58 +0530

    refactor(gogoanime_pe): fix link parsing

--- extensions/anime/gogoanime_pe.ts
+++ extensions/anime/gogoanime_pe.ts
@@ -3,7 +3,7 @@ import { ExtensionConfig } from "../../lib/model";
 export const config: ExtensionConfig = {
     name: "GogoAnime.pe",
     author: "zyrouge",
-    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/1cb05997f6c1d10fc520d84b3a64c2bfb96ae3bb/extensions/anime/gogoanime_pe/gogoanime_pe.ht",
-    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/1cb05997f6c1d10fc520d84b3a64c2bfb96ae3bb/extensions/anime/gogoanime_pe/gogoanime_pe.png",
+    source: "https://raw.githubusercontent.com/yukino-app/official-extensions/9f7f5e0094e555827817136d3a5706338e26f781/extensions/anime/gogoanime_pe/gogoanime_pe.ht",
+    image: "https://raw.githubusercontent.com/yukino-app/official-extensions/9f7f5e0094e555827817136d3a5706338e26f781/extensions/anime/gogoanime_pe/gogoanime_pe.png",
     nsfw: false,
 };
