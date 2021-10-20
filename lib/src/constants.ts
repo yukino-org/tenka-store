d07aa5697f698ee7e6bdde11f5c4adcbf7c520e4
ZYROUGE <zyrouge@hotmail.com>
2021-10-20 20:35:39 +0530

    chore: moved builder to seperate folder

--- lib/src/constants.ts
+++ lib/src/constants.ts
@@ -1,9 +1,9 @@
 import { join } from "path";
 
 export const paths = {
-    config: join(__dirname, "../extensions"),
-    dist: join(__dirname, "../dist"),
-    distExtensions: join(__dirname, "../dist/extensions"),
+    config: join(__dirname, "../../extensions"),
+    dist: join(__dirname, "../../dist"),
+    distExtensions: join(__dirname, "../../dist/extensions"),
     placeholder: join(__dirname, "../assets/placeholder.png"),
 };
 
